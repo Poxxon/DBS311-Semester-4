a5b32000568b244b1b15e0ff994917b3a51345c9
Poxxon <>
2025-02-10 18:04:47 -0500

    Procedure lesson

---
+++ In class/w6/week6.sql
@@ -0,0 +1,52 @@
+-- To display results on the terminal
+SET SERVEROUTPUT ON;
+
+-- Example 1 - Simple print
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Welcome');
+END;
+
+-- Example 2 - Variable and operations
+DECLARE
+VALUE1 number:=10;
+VALUE2 number:=20;
+RESULT NUMBER;
+
+BEGIN
+    RESULT:= VALUE1 + VALUE2;
+    DBMS_OUTPUT.PUT_LINE('The addition of two number is = ' || RESULT);
+END;
+
+-- Procedure: Addition - No return (void)
+CREATE OR REPLACE PROCEDURE ADDITION AS 
+VALUE1 number:=10;
+VALUE2 number:=20;
+RESULT NUMBER;
+BEGIN
+    RESULT:= VALUE1 + VALUE2;
+    DBMS_OUTPUT.PUT_LINE('The addition of two number is = ' || RESULT);
+END ADDITION;
+
+-- Procedure: Division - No return (void)
+CREATE OR REPLACE PROCEDURE DIVISION AS 
+VALUE1 number:=20;
+VALUE2 number:=10;
+RESULT NUMBER;
+BEGIN
+    RESULT:= VALUE1 / VALUE2;
+    DBMS_OUTPUT.PUT_LINE('The division of two number is = ' || RESULT);
+EXCEPTION
+    WHEN ZERO_DIVIDE
+    THEN
+    DBMS_OUTPUT.PUT_LINE('Check 2nd number');
+END DIVISION;
+
+-- Call procedure block (main function)
+BEGIN
+    ADDITION();
+    DIVISION();
+END;
+
+-- In class 
+SELECT * FROM EMPLOYEE_INFO;
+
